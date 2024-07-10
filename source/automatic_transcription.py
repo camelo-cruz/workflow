7a37a31d356a11304dd7fbc8d4dc9c3799b2e32a
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2024-07-10 13:21:43 +0200

    improving logics in automatic transcription without diarization

--- source/automatic_transcription.py
+++ source/automatic_transcription.py
@@ -25,7 +25,6 @@ import argparse
 import string
 import pandas as pd
 from pydub import AudioSegment
-from pyannote.audio import Pipeline
 from tqdm import tqdm
 from transliterate import translit
 import cutlet
@@ -34,10 +33,6 @@ import json
 
 warnings.filterwarnings("ignore")
 model = whisper.load_model("large-v3")
-pipeline = Pipeline.from_pretrained(
-    "pyannote/speaker-diarization-3.1",
-    use_auth_token="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")
-
 current_dir = os.getcwd()
 file_path = os.path.join(current_dir, 'materials', 'LANGUAGES')
 
@@ -91,41 +86,7 @@ def __cut_audio(audiofile, start, end):
     return output
 
 
-def __transcribe_with_diarization(audiofile, language):
-    """
-    Perform transcription with speaker diarization on an audio file.
-
-    Parameters:
-        audiofile (str): Path to the input audio file.
-
-    Returns:
-        list: List of dictionaries containing transcription information.
-    """
-    transcription = []
-    # diarize the audiofile
-    diarization = pipeline(audiofile)
-
-    # for each segment in diarization: get the whisper transcription
-    for turn, _, speaker in diarization.itertracks(yield_label=True):
-        audio_cut = __cut_audio(audiofile, turn.start, turn.end)
-        wh_trans = model.transcribe(audio_cut, language=language)
-        os.remove(audio_cut)
-
-        # add the whisper transcription to dict:transcription with the proper SpeakerID and timestamps
-        new_dict = {
-            'text': __process_string(wh_trans['text']),
-            'start': turn.start,
-            'end': turn.end,
-            'speaker': speaker
-        }
-
-        transcription.append(new_dict)
-
-    return transcription
-
-
-
-def process_data(directory, language, diarization = False, latin_transliteration = False):
+def process_data(directory, language, latin_transliteration = False):
     """
     Process audio files in a directory, perform transcription, and update a CSV file.
 
@@ -169,33 +130,10 @@ def process_data(directory, language, diarization = False, latin_transliteration
                 for file in tqdm(files, desc=f"Processing Files in subdir {subdir}", unit="file"):
                     if file.endswith('.mp3'):
                         count += 1
-                        # Use subdir as the base directory
-                        audio_file_path = os.path.join(subdir, file)
-                        
-                        # if 'automatic_transcription' not in df.columns:
-                        #     df['automatic_transcription'] = ""
-                        # if 'automatic_translation' not in df.columns:
-                        #     df['automatic_translation'] = ""
+                        audio_file_path = os.path.abspath(os.path.join(subdir, file))
                         transcription = ""
-                        if diarization:
-                            diarized_transcription = __transcribe_with_diarization(audio_file_path, language)
-                            add_speaker = lambda segment: f"{segment['speaker']}: {segment['text']} "
-                            for index in range(len(diarized_transcription)):
-                                segment = diarized_transcription[index]
-                                if index != 0:
-                                    previous_speaker = diarized_transcription[index - 1]["speaker"]
-                                    current_speaker = diarized_transcription[index]["speaker"]
-                                    same_speaker = previous_speaker == current_speaker
-                                    if same_speaker:
-                                        transcription += segment['text'] + " "
-                                    else:
-                                        transcription += add_speaker(segment)
-                                else:
-                                    transcription += add_speaker(segment)
-                        else:
-                            #if not diarization only transcribe
-                            transcription = model.transcribe(audio_file_path, language = language)
-                            transcription = __process_string(transcription["text"])
+                        transcription = model.transcribe(audio_file_path, language = language)
+                        transcription = __process_string(transcription["text"])
                         print(transcription)
 
                         series = df[df.isin([file])].stack()
@@ -227,7 +165,6 @@ def main():
     parser = argparse.ArgumentParser(description="automatic transcription")
     parser.add_argument("input_dir")
     parser.add_argument("language", default=None, help="Language of the audio content")
-    parser.add_argument("--diarization", action="store_true", help="Perform speaker diarization during transcription")
     parser.add_argument("--transliteration", action="store_true", help="Perform transliteration into latin alphabet for Japanese and Russian")
     args = parser.parse_args()
     
@@ -240,7 +177,7 @@ def main():
     else:
         print("No Language given. Language will automatically recognized")
         
-    process_data(args.input_dir, language, args.diarization, args.transliteration)
+    process_data(args.input_dir, language, args.transliteration)
 
 if __name__ == "__main__":
     main()
