cd065c91ed9b909a3752908d2d63cabf322b3555
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2024-07-17 14:44:36 +0200

    Added glossing for ukranian

--- source/automatic_glossing.py
+++ source/automatic_glossing.py
@@ -50,7 +50,7 @@ MODELS = {'de': {
           'ukr': {
               'translation':'Helsinki-NLP/opus-mt-uk-en', 
               'morphology':'uk_core_news_trf'
-              'prompt': []}
+              'prompt':[]}
           }
 
 def load_models(language_code):
