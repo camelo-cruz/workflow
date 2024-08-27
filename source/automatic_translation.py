a22597a3a04c373300210a2e06bd1d57fa0e8321
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2024-08-27 15:39:25 +0200

    Update automatic_translation.py

--- source/automatic_translation.py
+++ source/automatic_translation.py
@@ -26,21 +26,33 @@ import json
 
 current_dir = os.getcwd()
 file_path = os.path.join(current_dir, 'materials', 'LANGUAGES')
+nolatin_path = os.path.abspath(os.path.join(current_dir, 'materials', 'NO_LATIN'))
 
 with open(file_path, 'r', encoding='utf-8') as file:
     LANGUAGES = json.load(file)
 
+with open(nolatin_path, 'r', encoding='utf-8') as file:
+    NO_LATIN = file.read().splitlines()
+
 def translate(file, instruction, source_language):
     df = pd.read_excel(file)
+
+    automatic_column = "automatic_transcription"
+    corrected_column = "latin_transcription_everything"
+    sentences_column = "latin_transcription_utterance_used"
+
+    if source_language in NO_LATIN:
+        corrected_column = "transcription_original_script"
+        sentences_column = "transcription_original_script_utterance_used"
     
     for i in range(len(df)):
         try:
-            if instruction == 'corrected':
-                df.loc[i,"automatic_translation"] = GoogleTranslator(source=source_language, target='en').translate(df["latin_transcription_everything"][i])
+            if instruction == 'corrected_transcription':
+                df.loc[i,"automatic_translation"] = GoogleTranslator(source=source_language, target='en').translate(df[corrected_column][i])
             elif instruction == 'automatic_transcription':
-                df.loc[i,"automatic_translation"] = GoogleTranslator(source=source_language, target='en').translate(df["automatic_transcription"][i])
+                df.loc[i,"automatic_translation"] = GoogleTranslator(source=source_language, target='en').translate(df[automatic_column][i])
             elif instruction == 'sentences':
-                df.loc[i,"translation_utterance_used"] = GoogleTranslator(source=source_language, target='en').translate(df["latin_transcription_utterance_used"][i])
+                df.loc[i,"translation_utterance_used"] = GoogleTranslator(source=source_language, target='en').translate(df[sentences_column][i])
         except Exception as e:
             print(f"An error occurred: {str(e)}")
             
@@ -61,7 +73,7 @@ def main():
     parser = argparse.ArgumentParser(description="automatic transcription")
     parser.add_argument("input_dir")
     parser.add_argument("instruction", choices=["automatic_transcription", 
-                                                "corrected","sentences"], 
+                                                "corrected_transcription","sentences"], 
                         help="Type of instruction for processing.")
     parser.add_argument("source_language")
     args = parser.parse_args()
