74f6533c6c1b46fc77e431b708a68f775b4d3d3d
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2025-04-30 14:39:29 +0200

    debugging

--- tgt/urls.py
+++ tgt/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from django.http import HttpResponse
 
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    #path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('', lambda request: HttpResponse("<h1>✅ It works!</h1>")),
     path('process/', views.process, name='process'),
     path('logs/<str:job_id>', views.logs, name='logs'),
     path('cancel/<str:job_id>', views.cancel, name='cancel'),
