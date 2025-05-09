8a736a9e11dc421775d18094640ad717611c71de
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2025-05-09 12:14:46 +0200

    adding some refrac

--- tgt/urls.py
+++ tgt/urls.py
@@ -1,17 +1,22 @@
 from django.urls import path
 from django.views.generic import TemplateView
-
 from . import views
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="index.html"), name="index"),
-    path("process/",              views.process, name="process"),
-    path("stream/<str:job_id>/",  views.stream,  name="stream"),
-    path("cancel/",               views.cancel,  name="cancel"),
-    path("auth/start",            views.start_onedrive_auth,   name="start_onedrive_auth"),
-    path("auth/redirect",         views.onedrive_auth_redirect, name="onedrive_auth_redirect"),
-    path("auth/token",            views.get_access_token,       name="get_access_token"),
+    path("", views.index, name="index"),
+
+    # Path to the main page and authentication
+    path("auth/start", views.start_onedrive_auth, name="start_onedrive_auth"),
+    path("auth/redirect", views.onedrive_auth_redirect, name="onedrive_auth_redirect"),
+    path("auth/token", views.get_access_token, name="get_access_token"),
+
+    # Path to the process logics
+    path("process/", views.process, name="process"),
+    path("cancel/", views.cancel,  name="cancel"),
+    path("stream/<str:job_id>/", views.stream,  name="stream"),
     path('download/<str:job_id>/', views.download_zip, name='download_zip'),
+
+    # Path to the terms and privacy policy
     path("terms/", views.terms_view, name="terms"),
     path("privacy/", views.privacy_view, name="privacy"),
 ]
