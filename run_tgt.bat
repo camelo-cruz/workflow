f6cb50d44f8cb3ace28d890a52b16218205d481e
Alejandra Camelo Cruz <135133679+camelo-cruz@users.noreply.github.com>
2025-06-10 17:11:22 +0200

    changingbat

--- run_tgt.bat
+++ run_tgt.bat
@@ -10,7 +10,7 @@ cd /d C:\Users\camelo.cruz\Documents\GitHub\workflow\tgt
 
 :loop
 echo [%date% %time%] Starting Uvicorn... >> %LOGFILE%
-python.exe -m uvicorn app:app --host 127.0.0.1 --port 8000 --reload >> %LOGFILE% 2>&1
+python.exe -m uvicorn app:app --host 0.0.0.0 --port 8000 --reload >> %LOGFILE% 2>&1
 echo [%date% %time%] Uvicorn stopped. Restarting in 5s... >> %LOGFILE%
 timeout /t 5
 goto loop
