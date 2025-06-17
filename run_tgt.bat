f1b32820d0444dbec70455ec4b8ee9a74df09be9
Alejandra Camelo Cruz <135133679+camelo-cruz@users.noreply.github.com>
2025-06-17 12:16:11 +0200

    added new bat

--- run_tgt.bat
+++ run_tgt.bat
@@ -1,9 +1,20 @@
 @echo off
-title Uvicorn Server
+title Uvicorn + nginx Launcher
 set LOGFILE=%~dp0uvicorn-log.txt
 
 REM Activate the virtual environment
 CALL C:\Users\camelo.cruz\Documents\GitHub\workflow\tgt-venv\Scripts\activate.bat
+
+REM Start nginx in the background
+REM --------------------------------
+REM Option A: If you installed nginx by unzipping into C:\nginx\
+pushd C:\nginx
+start "" nginx.exe
+popd
+
+REM Option B: If you installed nginx as a Windows service
+REM net start nginx
+REM --------------------------------
 
 REM Change to project directory
 cd /d C:\Users\camelo.cruz\Documents\GitHub\workflow\tgt
@@ -14,4 +25,3 @@ python.exe -m uvicorn app:app --host 0.0.0.0 --port 8000 --reload >> %LOGFILE% 2
 echo [%date% %time%] Uvicorn stopped. Restarting in 5s... >> %LOGFILE%
 timeout /t 5
 goto loop
-
