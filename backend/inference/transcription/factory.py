43a56123f21e21595af3e02d62da80656a859fc4
Alejandra Camelo Cruz <135133679+camelo-cruz@users.noreply.github.com>
2025-07-07 21:56:26 +0200

    Update factory.py

--- backend/inference/transcription/factory.py
+++ backend/inference/transcription/factory.py
@@ -7,10 +7,10 @@ from inference.transcription.whisper import WhisperStrategy
 class TranscriptionStrategyFactory:
     @staticmethod
     def get_strategy(language_code: str) -> TranscriptionStrategy:
-        if language_code in ['en', 'fr', 'de', 'es', 'it', 'ja', 'nl', 'uk', 'pt', 'ar', 'cs',
+        if language_code in ['en', 'fr', 'de', 'es', 'it', 'ja', 'nl', 'uk', 'pt', 'cs',
                              'ru', 'pl', 'hu', 'fi', 'fa', 'el', 'tr', 'da', 'he', 'vi', 'ko',
                              'ur', 'te', 'hi', 'ca', 'ml', 'no', 'nn', 'sk', 'sl', 'hr', 'ro',
                              'eu', 'gl', 'ka', 'lv', 'tl', 'zh']:
             return WhisperxStrategy(language_code)
-        else:
+        elif language_code in ['ar']:
             return WhisperStrategy(language_code)
