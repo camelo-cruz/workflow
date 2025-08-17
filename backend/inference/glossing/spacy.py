db3075b20995e180de6aa2171585c16968802e3c
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2025-08-17 13:00:16 +0200

    adding things

--- backend/inference/glossing/spacy.py
+++ backend/inference/glossing/spacy.py
@@ -52,7 +52,7 @@ class SpaCyGlossingStrategy(GlossingStrategy):
         else:
             raise ValueError("No glossing model specified or available for this language.")
 
-    def gloss(self, text: str, keep_punct: bool = True, debug: bool = False) -> str:
+    def gloss(self, text: str, keep_punct: bool = True, debug: bool = True) -> str:
         """
         Build a Leipzig-style gloss string from model predictions.
         - Preserves newline boundaries: gloss each line independently.
